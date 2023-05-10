a548909deaa6474c7038406c5985bad1332a7ec6
YusufSener <yusuf_sener_97@hotmail.com>
2023-05-10 13:14:41 +0300

    edit movement

--- Assets/000Game/Scripts/PlayerMovement.cs
+++ Assets/000Game/Scripts/PlayerMovement.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [SerializeField] float _speed = 3f;
+    Vector3 _target;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+    }
 }
